b0e9e19078bcb7aefe56d6fa0c6d99a403108c76
Rob917 <50025116+Rob917@users.noreply.github.com>
2019-12-11 17:00:32 -0600

    Add project files.

---
+++ Advent/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Modules modules = new Modules();
+            var mod = modules.SpaceCraftModules();
+
+            var fuelXModule = new List<int>();
+
+            foreach (var item in mod)
+            {
+                var moduleFuel = (item / 3) - 2;
+                //var value = Convert.ToString(moduleFuel).Substring(1,5);
+                //fuelXModule.Add(Convert.ToInt32(value));
+                fuelXModule.Add(moduleFuel);
+            }
+
+            foreach (var item in mod)
+            {
+                var moduleFuel = (item / 3) - 2;
+
+                for (int i = 0; moduleFuel != 0; i++)
+                {
+
+                }
+                
+            }
+
+            int totalFuel = fuelXModule.Sum(x => Convert.ToInt32(x));
+
+            Console.WriteLine("Day 1: Sum fuel requirements for all modules is: " + totalFuel);
+            Console.ReadLine();
+        }
+    }
+}
