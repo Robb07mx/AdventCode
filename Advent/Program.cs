a4eb98c030ec909e523b532f4083621d50bf4f35
Rob917 <50025116+Rob917@users.noreply.github.com>
2019-12-12 16:47:26 -0600

    "Avance"

--- Advent/Program.cs
+++ Advent/Program.cs
@@ -16,6 +16,7 @@ namespace Advent
 
             var fuelXModule = new List<int>();
 
+            //Just for day 1
             foreach (var item in mod)
             {
                 var moduleFuel = (item / 3) - 2;
@@ -24,21 +25,40 @@ namespace Advent
                 fuelXModule.Add(moduleFuel);
             }
 
-            foreach (var item in mod)
-            {
-                var moduleFuel = (item / 3) - 2;
-
-                for (int i = 0; moduleFuel != 0; i++)
-                {
-
-                }
-                
-            }
-
             int totalFuel = fuelXModule.Sum(x => Convert.ToInt32(x));
 
             Console.WriteLine("Day 1: Sum fuel requirements for all modules is: " + totalFuel);
             Console.ReadLine();
+
+            var modulesTotalFuel = new List<int>();
+
+            foreach (var item in mod)
+            {
+                //List to store sum of total fuel needed per module
+                var singleModule = new List<int>();
+
+                var reqFuel = new List<int>();
+
+                for (int i = 0; i < length; i++)
+                {
+                    var fuelXMass = 0; //Variable initialized
+                    var fuelCalc = ((item/3)-2);
+                    if (fuelCalc == 0)
+                    {
+                        //if fuelcalc is zero then add value to the total fuel value needed (singleModule)
+                        singleModule.Add(fuelCalc);
+                    }
+                    else
+                    {
+                        var rec = ((fuelCalc / 3) - 2);
+                    }
+                }
+                
+              
+                
+            }
+
+            
         }
     }
 }
